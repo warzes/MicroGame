063d87d31b13920182434fbbeb832108d2d15fd3
warzes <warzes@mail.ru>
2022-11-08 23:54:10 +1000

    - подготовка нового теста

--- src/Game/LauncherApp.h
+++ src/Game/LauncherApp.h
@@ -7,6 +7,7 @@
 #	define TEST_1_TRIANGLES 0
 #	define TEST_2_TEXTUREQUADS 0
 #	define TEST_3_MODEL 1
+#	define TEST_4_MANYMODEL 0
 
 #	if TEST_1_TRIANGLES
 #		include "Test1Triangles.h"
@@ -18,6 +19,10 @@
 
 #	if TEST_3_MODEL
 #		include "Test3Model.h"
+#	endif
+
+#	if TEST_4_MANYMODEL
+#		include "Test4ManyModels.h"
 #	endif
 
 #endif // USE_TEST
