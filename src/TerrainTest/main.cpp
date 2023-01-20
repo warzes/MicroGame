2c7039b9ff623c2d6ffefb77f1b948586dbc6223
warzes <warzes@mail.ru>
2023-01-20 16:57:10 +1000

    - ссылка

--- src/TerrainTest/main.cpp
+++ src/TerrainTest/main.cpp
@@ -3,6 +3,8 @@
 #if defined(_MSC_VER)
 #	pragma comment( lib, "MicroEngine.lib" )
 #endif
+https://github.com/jing-interactive/melo
+
 та демка с пальмами
 https://trederia.blogspot.com/2014/09/water-in-opengl-and-gles-20-part-1.html
 https://github.com/AndrewMeshekoff/Portfolio/tree/master/Graphics/Procedural%20Islands
